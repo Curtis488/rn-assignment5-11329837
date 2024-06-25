a215df12533c1e9b822efa73c916d4893b8bb555
curtis488 <csnomensah@st.ug.edu.gh>
2024-06-25 13:37:08 +0000

    added images and finished profile pictureand name

--- CardApp/App.js
+++ CardApp/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Image style={styles.img1} source={require('./assets/photos/profile.png')} />
+      <Text style={styles.text1}>Welcome back,</Text>
+      <Text style={styles.text2}>Samuel Mensah</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img1:{
+    position: 'absolute',
+    top: 70,
+    left: 30,
+  },
+  text1:{
+    position: 'absolute',
+    top: 70,
+    left: 90,
+    fontSize: 16,
+    color: 'grey',
+  },
+  text2:{
+    position: 'absolute',
+    top: 95,
+    left: 90,
+    fontSize: 18,
   },
 });
